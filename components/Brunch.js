7e3a65121fa5292e3cfb67d2720520414c375e76
dreayoung <youngmandrea@gmail.com>
2022-09-09 10:36:26 -0400

    added yt video

--- components/Brunch.js
+++ components/Brunch.js
@@ -7,11 +7,22 @@ import zaeglover from '../public/zaeglover.png';
 const Brunch = ({ benefits }) => {
   return (
     <div className="pt-20 lg:pt-0">
-      <div className="h-[50vh] text-center pt-40 text-semiblack text-4xl">join us for brunch</div>
-      {/* <video className="w-full max-w-full h-auto rounded-lg border border-gray-200 dark:border-gray-700" controls autoPlay>
-  <source src={"../public/brunchvid.mp4"} type="video/mp4" />
-  Your browser does not support the video tag.
-</video> */}
+      <div className="w-full max-w-full h-auto">
+      <div className="lob text-goldie mt-20 text-center text-5xl lg:text-8xl rounded-lg border border-semiblack dark:border-gray-700">
+          join us for brunch
+        </div>
+        <div className="mt-8">
+        <iframe
+          width="100%"
+          height="500"
+          src="https://www.youtube.com/embed/ZTe9vHF_aLg"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        </div>
+      </div>
       <div className="items-center register pt-40">
         <h3 className="absolute rob font-bold uppercase leading-[0.6] left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-12 text-goldie text-5xl lg:text-6xl">
           Benefits
--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextConfig = {
   swcMinify: true,
 };
 
+const withVideos = require('next-videos');
+
+module.exports = withVideos({
+  assetPrefix: 'https://youtu.be/ZTe9vHF_aLg',
+
+  webpack(config, options) {
+    return config
+  }
+})
+
+
 module.exports = nextConfig;
