b018d40c1a05497f9ea21c2232947ea093ce0afb
dreayoung <youngmandrea@gmail.com>
2022-12-29 14:02:26 -0500

    faneto

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -8,14 +8,14 @@ import { BsFacebook } from 'react-icons/bs';
 function HeroSection() {
   return (
     <>
-      <div className="absolute right-0 top-[68%] lg:top-[20rem] lg:-bottom-72 lg:w-[60%] w-72">
+      <div className="absolute top-[68%] lg:top-[32rem] lg:-bottom-72 lg:w-[40%] w-full transform -translate-x-1/2 -tanslate-y-1/2 left-1/2">
         <Image src={woman} alt="woman svg" className="opacity-80 lg:opacity-60" />
       </div>
       <h1 className="relative brightness-110 my-10 text-[8vh] lg:text-[30vh] text-center text-goldie leading-[1.2] lg:leading-[0.8] uppercase rob">
         Bosses Academy
       </h1>
       <Link href="/register" className="cursor-pointer">
-      <div className="relative register mt-10 items-center justify-center text-pinky text-center animate-pulse">
+      <div className="backdrop-blur-3xl relative register mt-10 items-center justify-center text-pinky text-center animate-pulse">
         Register Today
       </div>
       </Link>
--- components/Structure.js
+++ components/Structure.js
@@ -15,10 +15,10 @@ const learnhowto = [
 
 const Structure = () => {
   return (
-    <section className={`${styles.main} overflow-x-hidden mt-10`}>
-      <div className="flex mt-40 mb-3">
-        <div className="relative border-t-[1px] border-t-semiblack w-80 lg:w-1/2 m-auto font-light text-goldie text-3xl lg:text-6xl items-center justify-center ">
-          <div className="rob font-bold uppercase lg:pr-2 text-center lg:pl-12 pt-40 pb-40 leading-[4rem]">
+    <section className={`${styles.main} overflow-x-hidden lg:mt-20 mt-60`}>
+      <div className="flex mt-20 mb-3">
+        <div className="relative w-80 lg:w-1/2 m-auto font-light text-goldie text-3xl lg:text-6xl items-center justify-center ">
+          <div className="rob font-bold uppercase lg:pr-2 text-center lg:pl-12 pt-40 pb-20 leading-[4rem]">
             <span className="hover:text-white">Structure.</span>
             <span className="hover:text-white"> Repair.</span>{' '}
             <span className="hover:text-white">Gain.</span>{' '}
@@ -28,7 +28,7 @@ const Structure = () => {
           </div>
         </div>
       </div>
-      <div className="pt-40 border-t-[1px] border-t-semiblack">
+      <div className="py-40 border-t-[1px] border-t-semiblack">
         <div className="items-center">
           <h3 className="absolute rob font-bold uppercase leading-[0.6] left-[13rem] whitespace-nowrap lg:left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-12 text-goldie text-3xl lg:text-6xl">
             Learn how to
