d03fd7585f527d9e14c3b7ea617a484cd8eb009b
dreayoung <38860105+dreayoung@users.noreply.github.com>
2024-06-03 06:55:12 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,17 @@ import Link from 'next/link';
 import ba from '../public/ba.png';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="p-4 bg-transparent rounded-lg shadow md:px-6 md:py-8">
       <div className="sm:flex sm:items-center sm:justify-between">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center mb-4 sm:mb-0"
-        >
+        <div className="flex items-center mb-4 sm:mb-0">
           <Image src={ba} width={50} height={50} alt="ba logo" />
           <span className="self-center pl-4 text-2xl font-semibold whitespace-nowrap font-beyulian text-pinky">
-            Bosses Academy 2022 All Rights Reserved.
+            {`©${currentYear} Bosses Academy. All Rights Reserved`}
           </span>
-        </a>
+        </div>
         <ul className="lg:flex lg:flex-wrap items-center  text-sm text-pinky register">
           <li className="mr-4 hover:underline md:mr-6 ">
             <Link href="/register">About</Link>
